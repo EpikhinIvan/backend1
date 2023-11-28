5b64f5d84e8eecc5014beb8dac5633941b6bd866
Norma Jean <cdm0971@gmail.com>
2023-11-28 07:15:21 +0600

    FAILED MODELS2

--- myapp/models.py
+++ myapp/models.py
@@ -19,6 +19,7 @@ class Application(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField() 
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=20, default='new') 
+    status = models.CharField(max_length=20, default='new')  
+   
     def __str__(self):
-        return f"{self.title} - {self.user.username}"
+        return "another fail"
